1d5bdc50770dcf5b5d2e6b76c006dcce6766d353
renzofernandezutec <renzo.fernandez@utec.edu.pe>
2022-03-29 00:25:41 -0500

    reverse function v1

--- circulararray.h
+++ circulararray.h
@@ -187,11 +187,24 @@ T& CircularArray<T>::operator[](int i) {
 
 // }
 
-// template <class T>
-// void CircularArray<T>::reverse() {
-
-// }
-
+template <class T>
+void CircularArray<T>::reverse() {
+	T *tmp = new T[capacity];
+	int contador = 0;
+	for(int i=back; i<lenght; i++) {
+		back = prev(back);
+		tmp[contador] = array[back];
+		cout << contador <<  " " << array[back] << endl;
+		cout << "back" << back << endl;
+		contador++;
+	}
+	// delete[] array;
+	array = tmp;
+	for(int i=0;i<capacity;i++)
+	{cout << endl;
+		cout << tmp[i] << " ";}
+	// delete[] tmp;
+}
 
 
 template <class T>
